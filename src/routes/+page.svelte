54faf066516c989ed4db03f68bd8f99c184c0f06
Soare Catalin <stevensuncatalin369@gmail.com>
2023-11-18 22:04:58 +0200

    added the payments page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount } from "svelte"
+
+  let token = '';
+
+  onMount(() => {
+    token = localStorage.getItem("token")
+  })
+</script>
+
+<div style="margin-top: 20vh">
+  <a href="/topup">TOPUP</a>
+
+  <h3>{token}</h3>
+</div>
